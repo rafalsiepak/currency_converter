c0449f02bf1449cefe907e0685a0d80216d98786
Rafał Siepak <rafal.siepak@outlook.com>
2022-10-29 14:45:49 +0200

    function update

--- js/script.js
+++ js/script.js
@@ -2,27 +2,31 @@ const calculateResult = (amount, currency) => {
     const result = amount * currency;
 };
 
-const init = () => {
+const updateResultText = (amount, result, currency) => {
+    const resultElement = document.querySelector(".js-result");
+    resultElement.innerText = result.toFixed(2);
+};
 
-const formElement = document.querySelector(".js-form");
-
-formElement.addEventListener("submit", (event) => {
+const onFormSubmit = (event) => {
     event.preventDefault();
 
     const amountElement = document.querySelector(".js-amount");
     const currencyElement = document.querySelector(".js-currency");
-    const resultElement = document.querySelector(".js-result");
 
     const amount = amountElement.value; 
     const currency = currencyElement.value;
 
     const result = calculateResult(amount, currency);
 
-    resultElement.innerText = result.toFixed(2);
+    updateResultText(amount, result, currency);
+};
 
+const init = () => {
 
-});
+    const formElement = document.querySelector(".js-form");
+    formElement.addEventListener("submit", onFormSubmit);
 
 };
+
 
 init();
