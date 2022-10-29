06ac2d3ed6ffbc2cbb284a23955a3db9eaca1e6d
Rafał Siepak <rafal.siepak@outlook.com>
2022-10-29 14:23:12 +0200

    currency calculator function

--- js/script.js
+++ js/script.js
@@ -1,15 +1,28 @@
-let amountElement = document.querySelector(".js-amount")
-let forexElement = document.querySelector(".js-forex")
-let formElement = document.querySelector(".js-form")
-let totalElement = document.querySelector(".js-total")
+const calculateResult = (amount, currency) => {
+    const result = amount * currency;
+};
+
+const init = () => {
+
+const formElement = document.querySelector(".js-form");
 
 formElement.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    let amount = amountElement.value; 
-    let forex = forexElement.value;
-    let total = amount * forex;
+    const amountElement = document.querySelector(".js-amount");
+    const currencyElement = document.querySelector(".js-currency");
+    const resultElement = document.querySelector(".js-result");
 
-    totalElement.innerText = total.toFixed(2);
+    const amount = amountElement.value; 
+    const currency = currencyElement.value;
+
+    const result = calculateResult(amount, currency);
+
+    resultElement.innerText = result.toFixed(2);
+
 
 });
+
+};
+
+init();
